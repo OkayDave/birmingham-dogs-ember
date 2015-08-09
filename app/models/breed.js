5102b03bbf9458ce0d1a4217517cc2c5120f954b
Dave Russell <dave.kerr@gmail.com>
2015-08-09 11:56:51 +0100

    Added breed index

--- app/models/breed.js
+++ app/models/breed.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr("string"),
   image_url: DS.attr("string"),
-  slug: DS.attr("slug"),
+  slug: DS.attr("string"),
 
   dogs: DS.hasMany('dog', {async: true, reverse: 'breed'})
   
